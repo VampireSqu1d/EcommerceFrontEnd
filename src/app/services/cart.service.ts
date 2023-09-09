febdb8a46e3922d5bff442349ce724454d2e18bf
Cuauhtemoc Ayala <vampiresqu1d@tutanota.com>
2023-09-08 19:11:26 -0700

    add to cart service and components created, add to cart functionality added on all products.

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem){
+    let alreadyExistsICart: boolean = false;
+    let existingCartItem: CartItem = undefined!;
+
+    if(this.cartItems.length > 0) {
+      
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id)!;
+
+      alreadyExistsICart = (existingCartItem != undefined);
+    }
+
+    if (alreadyExistsICart) {
+      existingCartItem.quantity++;
+    }
+    else {
+      this.cartItems.push(theCartItem);
+    }
+
+    this.computeCartTotals();
+
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for(let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.unitPrice * currentCartItem.quantity;
+      totalQuantityValue+= currentCartItem.quantity;
+    }
+
+    //publish the new values... all subscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log('contents of the cart');
+    for(let  cartItem of this.cartItems){
+      const subTotalPrice = cartItem.unitPrice * cartItem.quantity;
+      console.log(`name: ${cartItem.name}, 
+                  quantity: ${cartItem.quantity}, 
+                  unit Price: ${cartItem.unitPrice}, 
+                  subtotal price: ${subTotalPrice}`)
+
+      console.log(`total price: ${totalPriceValue.toFixed(2)}, total quantity: ${totalQuantityValue}`);
+      console.log('=======================================')
+    }
+  }
+}
